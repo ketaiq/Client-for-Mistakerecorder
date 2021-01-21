776cbbe794b5355f5ed70d40cc862a9e4cbb5fe2
CaSdm <38752093+uzktQ@users.noreply.github.com>
2021-01-21 19:16:14 +0800

    Initial files.

---
+++ mistakerecorder/mistakerecorder/mistakerecorderApp.swift
@@ -0,0 +1,17 @@
+//
+//  mistakerecorderApp.swift
+//  mistakerecorder
+//
+//  Created by CaSdm on 2021/1/21.
+//
+
+import SwiftUI
+
+@main
+struct mistakerecorderApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
