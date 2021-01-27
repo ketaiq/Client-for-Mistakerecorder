238c91431bacae88f1b098927e423dd0a05379be
CaSdm <38752093+uzktQ@users.noreply.github.com>
2021-01-27 15:43:58 +0800

    Add user menu view and relative animations.

--- mistakerecorder/mistakerecorder/HomeView.swift
+++ mistakerecorder/mistakerecorder/HomeView.swift
@@ -10,40 +10,60 @@ import SwiftUI
 struct HomeView: View {
     @State var unfoldMistakeCards = false
     @State var dragPosition = CGSize.zero
+    @State var showingUserMenuView = false
     var body: some View {
         NavigationView {
-            VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0, content: {
-                TitleView()
-                    .blur(radius: unfoldMistakeCards ? 20 : 0)
-                    .animation(.default)
-                Spacer()
-                ZStack {
-                    BackMistakeCardView()
-                        .offset(x: 0, y: unfoldMistakeCards ? -250 : -40)
-                        .offset(x: dragPosition.width, y: dragPosition.height)
-                        .scaleEffect(unfoldMistakeCards ? 0.6 : 0.9)
-                        .rotationEffect(.degrees(unfoldMistakeCards ? 0 : 10))
-                        .animation(.easeInOut(duration: 0.5))
-                    BackMistakeCardView()
-                        .offset(x: 0, y: unfoldMistakeCards ? -100 : -20)
-                        .offset(x: dragPosition.width, y: dragPosition.height)
-                        .scaleEffect(unfoldMistakeCards ? 0.8 : 0.95)
-                        .rotationEffect(.degrees(unfoldMistakeCards ? 0 : 5))
-                        .animation(.easeInOut(duration: 0.3))
-                    MistakeCardView(unfoldMistakeCards: $unfoldMistakeCards, dragPosition: $dragPosition)
-                        .offset(x: dragPosition.width, y: dragPosition.height)
-                        .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+            ZStack {
+                Color(#colorLiteral(red: 0.8039215803, green: 0.8039215803, blue: 0.8039215803, alpha: 1))
+                    .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
+                VStack(alignment: /*@START_MENU_TOKEN@*/.center/*@END_MENU_TOKEN@*/, spacing: 0, content: {
+                    TitleView(showingUserMenuView: $showingUserMenuView)
+                        .foregroundColor(.black)
+                        .blur(radius: unfoldMistakeCards ? 20 : 0)
+                        .animation(.default)
+                    ZStack {
+                        BackMistakeCardView()
+                            .offset(x: 0, y: unfoldMistakeCards ? -250 : -40)
+                            .offset(x: dragPosition.width, y: dragPosition.height)
+                            .scaleEffect(unfoldMistakeCards ? 0.6 : 0.9)
+                            .rotationEffect(.degrees(unfoldMistakeCards ? 0 : 10))
+                            .animation(.easeInOut(duration: 0.5))
+                        BackMistakeCardView()
+                            .offset(x: 0, y: unfoldMistakeCards ? -100 : -20)
+                            .offset(x: dragPosition.width, y: dragPosition.height)
+                            .scaleEffect(unfoldMistakeCards ? 0.8 : 0.95)
+                            .rotationEffect(.degrees(unfoldMistakeCards ? 0 : 5))
+                            .animation(.easeInOut(duration: 0.3))
+                        MistakeCardView(unfoldMistakeCards: $unfoldMistakeCards, dragPosition: $dragPosition)
+                            .offset(x: dragPosition.width, y: dragPosition.height)
+                            .animation(.spring(response: 0.3, dampingFraction: 0.6, blendDuration: 0))
+                        
+                    }
+                    .offset(y: 100)
+                    Spacer()
+                    HomeNavigationBar()
+                        .blur(radius: unfoldMistakeCards ? 20 : 0)
+                        .animation(.default)
+                        .padding(.bottom)
                     
-                }
+                })
+                .padding(.top, 44)
+                .padding(.horizontal)
+                .background(Color.white)
+                .clipShape(RoundedRectangle(cornerRadius: showingUserMenuView ? 30 : 0, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+                .shadow(color: Color.black.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+                .offset(y: showingUserMenuView ? -370 : 0)
+                .rotation3DEffect(
+                    Angle(degrees: showingUserMenuView ? -20 : 0),
+                    axis: (x: 1.0, y: 0.0, z: 0.0))
+                .scaleEffect(showingUserMenuView ? 0.8 : 1)
+                .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+                .edgesIgnoringSafeArea(/*@START_MENU_TOKEN@*/.all/*@END_MENU_TOKEN@*/)
                 
-                Spacer()
-                HomeNavigationBar()
-                    .blur(radius: unfoldMistakeCards ? 20 : 0)
-                    .animation(.default)
-            })
-            .padding()
-            .frame(maxWidth: .infinity, maxHeight: .infinity)
-            .background(Color.white)
+                UserMenuView()
+                    .offset(y: showingUserMenuView ? 0 : 600)
+                    .animation(.spring(response: 0.5, dampingFraction: 0.6, blendDuration: 0))
+            }
             .navigationBarHidden(true)
         }
     }
@@ -56,12 +76,21 @@ struct HomeView_Previews: PreviewProvider {
 }
 
 struct TitleView: View {
+    @Binding var showingUserMenuView: Bool
     var body: some View {
         HStack {
             Text("待复习错题")
                 .font(.largeTitle)
                 .fontWeight(/*@START_MENU_TOKEN@*/.bold/*@END_MENU_TOKEN@*/)
             Spacer()
+            Button(action: {
+                showingUserMenuView.toggle()
+            }, label: {
+                Image("ac84bcb7d0a20cf4800d77cc74094b36acaf990f")
+                    .resizable()
+                    .frame(width: 40, height: 40)
+                    .clipShape(/*@START_MENU_TOKEN@*/Circle()/*@END_MENU_TOKEN@*/)
+            })
         }
     }
 }
---
+++ mistakerecorder/mistakerecorder/UserMenuView.swift
@@ -0,0 +1,71 @@
+//
+//  UserMenuView.swift
+//  mistakerecorder
+//
+//  Created by CaSdm on 2021/1/27.
+//
+
+import SwiftUI
+
+struct UserMenuView: View {
+    var body: some View {
+        VStack {
+            Spacer()
+            VStack(spacing: 16) {
+                Text("邱珂泰 - 37% 已完成复习")
+                    .font(.caption)
+                Color.white
+                    .frame(width: 38, height: 6)
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .offset(x: -20, y: 0)
+                    .frame(width: 130, height: 6)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.08))
+                    .cornerRadius(/*@START_MENU_TOKEN@*/3.0/*@END_MENU_TOKEN@*/)
+                    .padding()
+                    .frame(width: 150, height: 24)
+                    .background(Color(#colorLiteral(red: 0, green: 0, blue: 0, alpha: 1)).opacity(0.1))
+                    .cornerRadius(12)
+                UserMenuRow(icon: "gear", title: "账户")
+                UserMenuRow(icon: "person.crop.circle.badge.minus", title: "退出登录")
+            }
+            .frame(maxWidth: .infinity)
+            .frame(height: 300)
+            .background(LinearGradient(gradient: Gradient(colors: [Color(#colorLiteral(red: 1, green: 1, blue: 1, alpha: 1)), Color(#colorLiteral(red: 0.2588235438, green: 0.7568627596, blue: 0.9686274529, alpha: 1))]), startPoint: .top, endPoint: .bottom))
+            .clipShape(RoundedRectangle(cornerRadius: 30, style: /*@START_MENU_TOKEN@*/.continuous/*@END_MENU_TOKEN@*/))
+            .shadow(color: Color/*@START_MENU_TOKEN@*/.black/*@END_MENU_TOKEN@*/.opacity(0.2), radius: 20, x: /*@START_MENU_TOKEN@*/0.0/*@END_MENU_TOKEN@*/, y: 20)
+            .padding(.horizontal, 30)
+            .overlay(
+                Image("ac84bcb7d0a20cf4800d77cc74094b36acaf990f")
+                    .resizable()
+                    .aspectRatio(contentMode: /*@START_MENU_TOKEN@*/.fill/*@END_MENU_TOKEN@*/)
+                    .frame(width: 60, height: 60)
+                    .clipShape(Circle())
+                    .offset(y: -150))
+        }
+        .padding(.bottom, 30)
+    }
+}
+
+struct UserMenuView_Previews: PreviewProvider {
+    static var previews: some View {
+        UserMenuView()
+    }
+}
+
+struct UserMenuRow: View {
+    var icon: String
+    var title: String
+    
+    var body: some View {
+        HStack(spacing: 16) {
+            Image(systemName: icon)
+                .font(.system(size: 20, weight: .light))
+                .imageScale(.large)
+                .frame(width: 32, height: 32)
+                .foregroundColor(Color(#colorLiteral(red: 0.6000000238, green: 0.6000000238, blue: 0.6000000238, alpha: 1)))
+            Text(title)
+                .font(.system(size: 20, weight: .bold, design: .default))
+                .frame(width: 120, alignment: .leading)
+        }
+    }
+} 
